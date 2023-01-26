c16f7f1577659a513ba30f71534b9d59934d3ba3
twmar <twmaryniak@gmail.com>
2023-01-25 19:59:21 -0500

    added comments

--- main2.py
+++ main2.py
@@ -6,6 +6,7 @@ import spotipy.util as util
 from time import sleep
 from datetime import datetime
 
+# Get Credentials from Creds.py file
 users = Creds.users
 playlist_id = Creds.playlist_id
 discord_webhook = Creds.discord_webhook
@@ -26,6 +27,7 @@ def request_add_on_discord(user_i):
     requests.post(discord_webhook, json=data)
 
 
+# Used to get correct authorization and scope from Spotify
 scope = 'playlist-modify-public user-library-modify playlist-modify-private user-top-read'
 os.environ["SPOTIPY_CLIENT_ID"] = Creds.ClientID
 os.environ["SPOTIPY_CLIENT_SECRET"] = Creds.SecretID
@@ -40,18 +42,20 @@ def get_token():
         print("Failed at receiving token")
 
 
+# 'logs in' to spotfiy and gets client
 spotify = spotipy.Spotify(auth=get_token())
 
+# Retrieves to total number of songs in playlist to be used in offset
 items = spotify.playlist_items(playlist_id, limit=5)
-
 total = items["total"]
 sleep(1)
 
-items = spotify.playlist_items(playlist_id, offset=total-20)
+# Get the last 20 songs and extracts useful information from playlist
+items = spotify.playlist_items(playlist_id, offset=total - 20)
 songs = list(get_track_info(x) for x in items["items"])
 
+# iterates over users to figure out who has not added a songs for the longest time
 songs_rev_who = [x["who"] for x in songs[::-1]]
-
 last = 0
 next_i = 0
 for i, name in enumerate([x[0] for x in users]):
@@ -59,5 +63,5 @@ for i, name in enumerate([x[0] for x in users]):
         next_i = i
         last = songs_rev_who.index(name)
 
-
+# Posts notification on Discord
 request_add_on_discord(next_i)
